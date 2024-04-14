2c1d39b7787f8241c6d64a7ad7db836806a63dc2
Dylan Lanigan-Smith <dylanlanigansmith@gmail.com>
2024-04-13 21:27:28 -0400

    works in vbox but not with vbox uefi for likely reasons

--- kernel/stdlib/memory.c
+++ kernel/stdlib/memory.c
@@ -14,16 +14,27 @@ void* memset(void* dest, register uint8_t val, register size_t num_bytes){
     return dest;
 }
 
-void* memset_old(void* dest, uint8_t val, size_t num_bytes){
+void* memset_old(void* dest, uint8_t val, size_t num_bytes){ 
+    //so i remember it being like super late and i wrote this like when i first had C going just to test something quick
+
+    //anyhow it ended up staying for a LONG time 
+    //and it is so shitty and since stuff goes on stack
+    //can get totally screwed by interupts 
+    //google memset async signal safe 
+    //anyways the register keyword make da problems go away
+    //keeping this here to remind myself not to write shitty code 
+    //doubt it will work 
     for(int i = 0; i < num_bytes; ++i){ //dunno
         *((char*)(dest + i)) = val;
     }
     return dest;
 }
 
+
+//REWRITE THIS HOLY 
 void *memset_u32(volatile uint32_t* dest, const uint32_t val, const size_t num_ints) //so we pass size as number of 4 byte chunks to write
 {
-    __asm__ volatile ("cli");
+    __asm__ volatile ("cli"); //todo: investigate if we need this w/ registers 
     //todo alignment
     if(!dest || !num_ints) return nullptr;
     
